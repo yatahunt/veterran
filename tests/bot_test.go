ea863c36a9b55e0c3e57bf1c063c22443951481b
AiSee <aiseeq@gmail.com>
2019-02-12 21:56:26 +0200

    Benchmark_Pathing done

--- tests/bot_test.go
+++ tests/bot_test.go
@@ -8,10 +8,15 @@ import (
 	"testing"
 )
 
+// go test -v -run=^$ -bench=^Benchmark_Pathing$ -benchtime=2s -cpuprofile=cpu.prof
+// go test -v -run=^$ -bench=^Benchmark_Pathing$ -benchtime=2s -memprofile=mem.prof
+// go tool pprof tests.test.exe cpu.prof
+// go tool pprof tests.test.exe mem.prof
+
 func Benchmark_Pathing(b *testing.B) {
 	B := bot.B
 	B.LoadState()
-	B.Locs.MyStart = point.Pt2(B.Info.GameInfo().StartRaw.StartLocations[1])
+	B.Locs.MyStart = point.Pt3(B.Info.Observation().Observation.RawData.Player.Camera)
 	B.Locs.EnemyStart = point.Pt2(B.Info.GameInfo().StartRaw.StartLocations[0])
 
 	B.Grid = grid.New(B.Info.GameInfo().StartRaw, B.Info.Observation().Observation.RawData.MapState)
